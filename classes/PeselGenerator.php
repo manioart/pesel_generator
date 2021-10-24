28a13ba387e244e2539823baf9d58966f1407117
Mariusz <swierczek-mariusz@o2.pl>
2021-10-24 10:39:37 +0200

    control digit

--- classes/PeselGenerator.php
+++ classes/PeselGenerator.php
@@ -31,8 +31,33 @@ class PeselGenerator {
             else {
                 $generatedPesel .= $odd;
             }
+            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
 
-            var_dump($generatedPesel);
+            $singleDigitsArray[0] *= 1;
+            $singleDigitsArray[1] *= 3;
+            $singleDigitsArray[2] *= 7;
+            $singleDigitsArray[3] *= 9;
+            $singleDigitsArray[4] *= 1;
+            $singleDigitsArray[5] *= 3;
+            $singleDigitsArray[6] *= 7;
+            $singleDigitsArray[7] *= 9;
+            $singleDigitsArray[8] *= 1;
+            $singleDigitsArray[9] *= 3;
+
+            for ($i = 0; $i < count($singleDigitsArray); $i++) {
+                if (strlen($singleDigitsArray[$i]) >= 2) {
+                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
+                }
+            }
+            $controlDigit = array_sum($singleDigitsArray);
+
+            if (strlen($controlDigit) >= 2) {
+                $controlDigit = 10 - (int)substr($controlDigit, 1);
+            }
+
+            $generatedPesel .= (string)$controlDigit;
+
+            return $generatedPesel;
         }
         elseif ($dateOfBirth > $date19 && $dateOfBirth < $date20) {
             $generatedPesel = substr($yearOfBirth,-2);
@@ -47,7 +72,33 @@ class PeselGenerator {
             else {
                 $generatedPesel .= $odd;
             }
-            var_dump($generatedPesel);
+            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
+
+            $singleDigitsArray[0] *= 1;
+            $singleDigitsArray[1] *= 3;
+            $singleDigitsArray[2] *= 7;
+            $singleDigitsArray[3] *= 9;
+            $singleDigitsArray[4] *= 1;
+            $singleDigitsArray[5] *= 3;
+            $singleDigitsArray[6] *= 7;
+            $singleDigitsArray[7] *= 9;
+            $singleDigitsArray[8] *= 1;
+            $singleDigitsArray[9] *= 3;
+
+            for ($i = 0; $i < count($singleDigitsArray); $i++) {
+                if (strlen($singleDigitsArray[$i]) >= 2) {
+                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
+                }
+            }
+            $controlDigit = array_sum($singleDigitsArray);
+
+            if (strlen($controlDigit) >= 2) {
+                $controlDigit = 10 - (int)substr($controlDigit, 1);
+            }
+
+            $generatedPesel .= (string)$controlDigit;
+
+            return $generatedPesel;
         }
         elseif ($dateOfBirth > $date20 && $dateOfBirth < $date21) {
             $generatedPesel = substr($yearOfBirth,-2);
@@ -62,7 +113,33 @@ class PeselGenerator {
             else {
                 $generatedPesel .= $odd;
             }
-            var_dump($generatedPesel);
+            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
+
+            $singleDigitsArray[0] *= 1;
+            $singleDigitsArray[1] *= 3;
+            $singleDigitsArray[2] *= 7;
+            $singleDigitsArray[3] *= 9;
+            $singleDigitsArray[4] *= 1;
+            $singleDigitsArray[5] *= 3;
+            $singleDigitsArray[6] *= 7;
+            $singleDigitsArray[7] *= 9;
+            $singleDigitsArray[8] *= 1;
+            $singleDigitsArray[9] *= 3;
+
+            for ($i = 0; $i < count($singleDigitsArray); $i++) {
+                if (strlen($singleDigitsArray[$i]) >= 2) {
+                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
+                }
+            }
+            $controlDigit = array_sum($singleDigitsArray);
+
+            if (strlen($controlDigit) >= 2) {
+                $controlDigit = 10 - (int)substr($controlDigit, 1);
+            }
+
+            $generatedPesel .= (string)$controlDigit;
+
+            return $generatedPesel;
         }
         elseif ($dateOfBirth > $date21 && $dateOfBirth < $date22) {
             $generatedPesel = substr($yearOfBirth,-2);
@@ -77,7 +154,33 @@ class PeselGenerator {
             else {
                 $generatedPesel .= $odd;
             }
-            var_dump($generatedPesel);
+            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
+
+            $singleDigitsArray[0] *= 1;
+            $singleDigitsArray[1] *= 3;
+            $singleDigitsArray[2] *= 7;
+            $singleDigitsArray[3] *= 9;
+            $singleDigitsArray[4] *= 1;
+            $singleDigitsArray[5] *= 3;
+            $singleDigitsArray[6] *= 7;
+            $singleDigitsArray[7] *= 9;
+            $singleDigitsArray[8] *= 1;
+            $singleDigitsArray[9] *= 3;
+
+            for ($i = 0; $i < count($singleDigitsArray); $i++) {
+                if (strlen($singleDigitsArray[$i]) >= 2) {
+                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
+                }
+            }
+            $controlDigit = array_sum($singleDigitsArray);
+
+            if (strlen($controlDigit) >= 2) {
+                $controlDigit = 10 - (int)substr($controlDigit, 1);
+            }
+
+            $generatedPesel .= (string)$controlDigit;
+
+            return $generatedPesel;
         }
         elseif ($dateOfBirth > $date22 && $dateOfBirth < $dateEnds) {
             $generatedPesel = substr($yearOfBirth,-2);
@@ -92,7 +195,33 @@ class PeselGenerator {
             else {
                 $generatedPesel .= $odd;
             }
-            var_dump($generatedPesel);
+            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
+
+            $singleDigitsArray[0] *= 1;
+            $singleDigitsArray[1] *= 3;
+            $singleDigitsArray[2] *= 7;
+            $singleDigitsArray[3] *= 9;
+            $singleDigitsArray[4] *= 1;
+            $singleDigitsArray[5] *= 3;
+            $singleDigitsArray[6] *= 7;
+            $singleDigitsArray[7] *= 9;
+            $singleDigitsArray[8] *= 1;
+            $singleDigitsArray[9] *= 3;
+
+            for ($i = 0; $i < count($singleDigitsArray); $i++) {
+                if (strlen($singleDigitsArray[$i]) >= 2) {
+                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
+                }
+            }
+            $controlDigit = array_sum($singleDigitsArray);
+
+            if (strlen($controlDigit) >= 2) {
+                $controlDigit = 10 - (int)substr($controlDigit, 1);
+            }
+
+            $generatedPesel .= (string)$controlDigit;
+
+            return $generatedPesel;
         }
         elseif ($dateOfBirth > $dateEnds) {
             return 'Nie przewidziano jeszcze numeru pesel dla podanej daty urodzenia';
