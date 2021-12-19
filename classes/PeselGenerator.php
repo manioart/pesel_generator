9d76c09b3a20a506f27d498821f63e5f87a169d0
Mariusz <swierczek-mariusz@o2.pl>
2021-12-19 17:47:02 +0100

    Remove repeated code

--- classes/PeselGenerator.php
+++ classes/PeselGenerator.php
@@ -2,7 +2,7 @@
 
 class PeselGenerator {
 
-    public static function generatePeselNumber($dateOfBirth,$sex) {
+    public static function generateFullPeselNumber($dateOfBirth,$sex) {
 
         $dateOfBirth = date('Y-m-d',strtotime($dateOfBirth));
         $yearOfBirth = date('Y',strtotime($dateOfBirth));
@@ -21,206 +21,32 @@ class PeselGenerator {
         elseif ($dateOfBirth > $date18 && $dateOfBirth < $date19) {
             $generatedPesel = substr($yearOfBirth,-2);
             $generatedPesel .= (string)((int)$monthOfBirth + 80);
-            $generatedPesel .= $dayOfBirth;
-            $randomValue = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
-            $even = substr($randomValue,0, 3) . substr($randomValue,-1) & ~1;
-            $odd = substr($randomValue,0, 3) . substr($randomValue,-1) | 1;
-            if ($sex == 'woman') {
-                $generatedPesel .= $even;
-            }
-            else {
-                $generatedPesel .= $odd;
-            }
-            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
-
-            $singleDigitsArray[0] *= 1;
-            $singleDigitsArray[1] *= 3;
-            $singleDigitsArray[2] *= 7;
-            $singleDigitsArray[3] *= 9;
-            $singleDigitsArray[4] *= 1;
-            $singleDigitsArray[5] *= 3;
-            $singleDigitsArray[6] *= 7;
-            $singleDigitsArray[7] *= 9;
-            $singleDigitsArray[8] *= 1;
-            $singleDigitsArray[9] *= 3;
-
-            for ($i = 0; $i < count($singleDigitsArray); $i++) {
-                if (strlen($singleDigitsArray[$i]) >= 2) {
-                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
-                }
-            }
-            $controlDigit = array_sum($singleDigitsArray);
-
-            if (strlen($controlDigit) >= 2) {
-                $controlDigit = 10 - (int)substr($controlDigit, 1);
-            }
-
-            $generatedPesel .= (string)$controlDigit;
-
+            $generatedPesel = PeselGenerator::generateEndPartOfPesel($generatedPesel,$dayOfBirth,$sex);
             return $generatedPesel;
+
         }
         elseif ($dateOfBirth > $date19 && $dateOfBirth < $date20) {
             $generatedPesel = substr($yearOfBirth,-2);
             $generatedPesel .= $monthOfBirth;
-            $generatedPesel .= $dayOfBirth;
-            $randomValue = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
-            $even = substr($randomValue,0, 3) . substr($randomValue,-1) & ~1;
-            $odd = substr($randomValue,0, 3) . substr($randomValue,-1) | 1;
-            if ($sex == 'woman') {
-                $generatedPesel .= $even;
-            }
-            else {
-                $generatedPesel .= $odd;
-            }
-            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
-
-            $singleDigitsArray[0] *= 1;
-            $singleDigitsArray[1] *= 3;
-            $singleDigitsArray[2] *= 7;
-            $singleDigitsArray[3] *= 9;
-            $singleDigitsArray[4] *= 1;
-            $singleDigitsArray[5] *= 3;
-            $singleDigitsArray[6] *= 7;
-            $singleDigitsArray[7] *= 9;
-            $singleDigitsArray[8] *= 1;
-            $singleDigitsArray[9] *= 3;
-
-            for ($i = 0; $i < count($singleDigitsArray); $i++) {
-                if (strlen($singleDigitsArray[$i]) >= 2) {
-                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
-                }
-            }
-            $controlDigit = array_sum($singleDigitsArray);
-
-            if (strlen($controlDigit) >= 2) {
-                $controlDigit = 10 - (int)substr($controlDigit, 1);
-            }
-
-            $generatedPesel .= (string)$controlDigit;
-
+            $generatedPesel = PeselGenerator::generateEndPartOfPesel($generatedPesel,$dayOfBirth,$sex);
             return $generatedPesel;
         }
         elseif ($dateOfBirth > $date20 && $dateOfBirth < $date21) {
             $generatedPesel = substr($yearOfBirth,-2);
             $generatedPesel .= (string)((int)$monthOfBirth + 20);
-            $generatedPesel .= $dayOfBirth;
-            $randomValue = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
-            $even = substr($randomValue,0, 3) . substr($randomValue,-1) & ~1;
-            $odd = substr($randomValue,0, 3) . substr($randomValue,-1) | 1;
-            if ($sex == 'woman') {
-                $generatedPesel .= $even;
-            }
-            else {
-                $generatedPesel .= $odd;
-            }
-            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
-
-            $singleDigitsArray[0] *= 1;
-            $singleDigitsArray[1] *= 3;
-            $singleDigitsArray[2] *= 7;
-            $singleDigitsArray[3] *= 9;
-            $singleDigitsArray[4] *= 1;
-            $singleDigitsArray[5] *= 3;
-            $singleDigitsArray[6] *= 7;
-            $singleDigitsArray[7] *= 9;
-            $singleDigitsArray[8] *= 1;
-            $singleDigitsArray[9] *= 3;
-
-            for ($i = 0; $i < count($singleDigitsArray); $i++) {
-                if (strlen($singleDigitsArray[$i]) >= 2) {
-                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
-                }
-            }
-            $controlDigit = array_sum($singleDigitsArray);
-
-            if (strlen($controlDigit) >= 2) {
-                $controlDigit = 10 - (int)substr($controlDigit, 1);
-            }
-
-            $generatedPesel .= (string)$controlDigit;
-
+            $generatedPesel = PeselGenerator::generateEndPartOfPesel($generatedPesel,$dayOfBirth,$sex);
             return $generatedPesel;
         }
         elseif ($dateOfBirth > $date21 && $dateOfBirth < $date22) {
             $generatedPesel = substr($yearOfBirth,-2);
             $generatedPesel .= (string)((int)$monthOfBirth + 40);
-            $generatedPesel .= $dayOfBirth;
-            $randomValue = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
-            $even = substr($randomValue,0, 3) . substr($randomValue,-1) & ~1;
-            $odd = substr($randomValue,0, 3) . substr($randomValue,-1) | 1;
-            if ($sex == 'woman') {
-                $generatedPesel .= $even;
-            }
-            else {
-                $generatedPesel .= $odd;
-            }
-            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
-
-            $singleDigitsArray[0] *= 1;
-            $singleDigitsArray[1] *= 3;
-            $singleDigitsArray[2] *= 7;
-            $singleDigitsArray[3] *= 9;
-            $singleDigitsArray[4] *= 1;
-            $singleDigitsArray[5] *= 3;
-            $singleDigitsArray[6] *= 7;
-            $singleDigitsArray[7] *= 9;
-            $singleDigitsArray[8] *= 1;
-            $singleDigitsArray[9] *= 3;
-
-            for ($i = 0; $i < count($singleDigitsArray); $i++) {
-                if (strlen($singleDigitsArray[$i]) >= 2) {
-                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
-                }
-            }
-            $controlDigit = array_sum($singleDigitsArray);
-
-            if (strlen($controlDigit) >= 2) {
-                $controlDigit = 10 - (int)substr($controlDigit, 1);
-            }
-
-            $generatedPesel .= (string)$controlDigit;
-
+            $generatedPesel = PeselGenerator::generateEndPartOfPesel($generatedPesel,$dayOfBirth,$sex);
             return $generatedPesel;
         }
         elseif ($dateOfBirth > $date22 && $dateOfBirth < $dateEnds) {
             $generatedPesel = substr($yearOfBirth,-2);
             $generatedPesel .= (string)((int)$monthOfBirth + 60);
-            $generatedPesel .= $dayOfBirth;
-            $randomValue = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
-            $even = substr($randomValue,0, 3) . substr($randomValue,-1) & ~1;
-            $odd = substr($randomValue,0, 3) . substr($randomValue,-1) | 1;
-            if ($sex == 'woman') {
-                $generatedPesel .= $even;
-            }
-            else {
-                $generatedPesel .= $odd;
-            }
-            $singleDigitsArray = array_map('intval',str_split($generatedPesel));
-
-            $singleDigitsArray[0] *= 1;
-            $singleDigitsArray[1] *= 3;
-            $singleDigitsArray[2] *= 7;
-            $singleDigitsArray[3] *= 9;
-            $singleDigitsArray[4] *= 1;
-            $singleDigitsArray[5] *= 3;
-            $singleDigitsArray[6] *= 7;
-            $singleDigitsArray[7] *= 9;
-            $singleDigitsArray[8] *= 1;
-            $singleDigitsArray[9] *= 3;
-
-            for ($i = 0; $i < count($singleDigitsArray); $i++) {
-                if (strlen($singleDigitsArray[$i]) >= 2) {
-                    $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
-                }
-            }
-            $controlDigit = array_sum($singleDigitsArray);
-
-            if (strlen($controlDigit) >= 2) {
-                $controlDigit = 10 - (int)substr($controlDigit, 1);
-            }
-
-            $generatedPesel .= (string)$controlDigit;
-
+            $generatedPesel = PeselGenerator::generateEndPartOfPesel($generatedPesel,$dayOfBirth,$sex);
             return $generatedPesel;
         }
         elseif ($dateOfBirth > $dateEnds) {
@@ -228,4 +54,45 @@ class PeselGenerator {
         }
 
     }
+
+    public static function generateEndPartOfPesel($generatedPesel,$dayOfBirth,$sex) {
+
+        $generatedPesel .= $dayOfBirth;
+        $randomValue = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
+        $even = substr($randomValue,0, 3) . substr($randomValue,-1) & ~1;
+        $odd = substr($randomValue,0, 3) . substr($randomValue,-1) | 1;
+        if ($sex == 'woman') {
+            $generatedPesel .= $even;
+        }
+        else {
+            $generatedPesel .= $odd;
+        }
+        $singleDigitsArray = array_map('intval',str_split($generatedPesel));
+
+        $singleDigitsArray[0] *= 1;
+        $singleDigitsArray[1] *= 3;
+        $singleDigitsArray[2] *= 7;
+        $singleDigitsArray[3] *= 9;
+        $singleDigitsArray[4] *= 1;
+        $singleDigitsArray[5] *= 3;
+        $singleDigitsArray[6] *= 7;
+        $singleDigitsArray[7] *= 9;
+        $singleDigitsArray[8] *= 1;
+        $singleDigitsArray[9] *= 3;
+
+        for ($i = 0; $i < count($singleDigitsArray); $i++) {
+            if (strlen($singleDigitsArray[$i]) >= 2) {
+                $singleDigitsArray[$i] = (int)substr($singleDigitsArray[$i], 1);
+            }
+        }
+        $controlDigit = array_sum($singleDigitsArray);
+
+        if (strlen($controlDigit) >= 2) {
+            $controlDigit = 10 - (int)substr($controlDigit, 1);
+        }
+
+        $generatedPesel .= (string)$controlDigit;
+
+        return $generatedPesel;
+   }
 }
